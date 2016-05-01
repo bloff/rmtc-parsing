63968ae9e543be88cd2864d582edd491bfbf2e0a
Bruno Loff <bloff.sites@gmail.com>
2016-05-01 11:31:57 +0200

    For some reason the imports in anoky.module had disappeared.

--- anoky/module.py
+++ anoky/module.py
@@ -1,7 +1,1 @@
-#from anoky.expansion
-#from anoky.macros.Quote import Quote
-
-
-#from anoky.generation
-#from anoky.special_forms.special_forms import
-#from anoky.special_forms.Import import
+from anoky.syntax import *
