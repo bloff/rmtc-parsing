3b6396666118b805200fe303d5a9422442d5eed2
Bruno Loff <bloff.sites@gmail.com>
2016-06-12 09:36:19 +0200

    fixed apply_to_rest transducer

--- anoky/transducers/arrangements/apply_to_rest.py
+++ anoky/transducers/arrangements/apply_to_rest.py
@@ -22,19 +22,23 @@ class ApplyToRest(ArrangementRule):
     def applies(self, element:Element):
         return (is_identifier(element.code) and
                 element.code.name in self.names and
-                (element.parent.__class__ is not Form or not element.is_first()))
+                (element.parent.__class__ is not Form or not element.is_first() or len(element.parent) > 2))
 
     def apply(self, element):
         form = element.parent
-        if not element.is_last():
-            new_form = form.wrap(element.next, form.last, Form).code
-            form.remove(element)
-            new_form.prepend(element.code)
-        else:
-            new_form = form.wrap(element, element, Form).code
-            head = new_form.first.code
-            new_form.remove(new_form.first)
-            new_form.prepend(head)
+        if element.parent.__class__ is not Form or not element.is_first():
+                if not element.is_last():
+                    new_form = form.wrap(element.next, form.last, Form).code
+                    form.remove(element)
+                    new_form.prepend(element.code)
+                else:
+                    new_form = form.wrap(element, element, Form).code
+                    head = new_form.first.code
+                    new_form.remove(new_form.first)
+                    new_form.prepend(head)
+        elif element.parent.__class__ is Form and len(element.parent) > 2:
+            form.wrap(element.next, form.last, PreForm).code
+
         return None
 
 
